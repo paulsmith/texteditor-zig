a56e025d847fc30c7d14ea8e7b22c606933059ce
Paul Smith <paulsmith@gmail.com>
2021-01-11 19:49:13 -0600

    Step 3

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,10 @@
-const std = @import("std");
+const io = @import("std").io;
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    const stdin = io.getStdIn().reader();
+    var buf: [1]u8 = undefined;
+    while (true) {
+        const n = try stdin.read(buf[0..]);
+        if (n != 1) break;
+    }
 }
