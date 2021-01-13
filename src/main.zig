028637c2fbff058cf0f9f35accb55559a3df98ff
Paul Smith <paulsmith@gmail.com>
2021-01-13 11:45:25 -0600

    Step 10

--- src/main.zig
+++ src/main.zig
@@ -26,6 +26,7 @@ fn enableRawMode() !void {
     orig_termios = try os.tcgetattr(stdin_fd);
     try atexit(disableRawMode);
     var raw = orig_termios;
+    raw.iflag &= ~@as(os.tcflag_t, os.IXON);
     raw.lflag &= ~@as(os.tcflag_t, os.ECHO | os.ICANON | os.ISIG);
     try os.tcsetattr(stdin_fd, os.TCSA.FLUSH, raw);
 }
