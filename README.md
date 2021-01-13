7f23881c4fa6fc1d7d2cc0091f279342142f38be
Paul Smith <paulsmith@gmail.com>
2021-01-13 08:52:32 -0600

    Step 5

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 https://viewsourcecode.org/snaptoken/kilo/
+
+If using macOS, requires at least Zig git checkout a021c7b1.
--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,19 @@
 const io = @import("std").io;
+const os = @import("std").os;
 
 pub fn main() anyerror!void {
+    try enableRawMode();
     const stdin = io.getStdIn().reader();
     var buf: [1]u8 = undefined;
     while (true) {
         const n = try stdin.read(buf[0..]);
         if (n != 1 or buf[0] == 'q') break;
     }
+}
+
+fn enableRawMode() !void {
+    const fd = io.getStdIn().handle;
+    var raw = try os.tcgetattr(fd);
+    raw.lflag &= ~@as(os.tcflag_t, os.ECHO);
+    try os.tcsetattr(fd, os.TCSA.FLUSH, raw);
 }
