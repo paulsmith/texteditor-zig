99d24b795c4ac010f16604cec7ec9d4608d45843
Paul Smith <paulsmith@gmail.com>
2021-01-13 10:43:30 -0600

    Implement atexit in Zig

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,5 @@
 const io = @import("std").io;
 const os = @import("std").os;
-const c = @cImport(@cInclude("stdlib.h"));
 
 pub fn main() anyerror!void {
     try enableRawMode();
@@ -17,7 +16,7 @@ const stdin_fd = io.getStdIn().handle;
 
 fn enableRawMode() !void {
     orig_termios = try os.tcgetattr(stdin_fd);
-    if (c.atexit(disableRawMode) == -1) return error.AtExit;
+    try atexit(disableRawMode);
     var raw = orig_termios;
     raw.lflag &= ~@as(os.tcflag_t, os.ECHO);
     try os.tcsetattr(stdin_fd, os.TCSA.FLUSH, raw);
@@ -25,4 +24,25 @@ fn enableRawMode() !void {
 
 export fn disableRawMode() void {
     os.tcsetattr(stdin_fd, os.TCSA.FLUSH, orig_termios) catch unreachable;
+}
+
+// TODO(paulsmith): delete all the following when added to zig's lib/std/os.zig
+
+const AtExitError = error{NoMemoryAvailable} || os.UnexpectedError;
+
+// TODO(paulsmith): delete this when added to zig's lib/std/c.zig
+const _c = struct {
+    pub extern fn atexit(function: ?fn () callconv(.C) void) c_int;
+};
+
+fn atexit(comptime function_ptr: anytype) AtExitError!void {
+    const ti = @typeInfo(@TypeOf(function_ptr));
+    if (ti.Fn.return_type.? != void or ti.Fn.args.len != 0 or ti.Fn.calling_convention != .C) {
+        @compileError("atexit registered function must have return type void, take no args, and use C calling convention");
+    }
+    switch (os.errno(_c.atexit(function_ptr))) {
+        0 => return,
+        os.ENOMEM => return AtExitError.NoMemoryAvailable,
+        else => |err| return os.unexpectedErrno(err),
+    }
 }
