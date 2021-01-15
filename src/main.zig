f8e40fc766b7c8857ec27189a9829e486659ec92
Paul Smith <paulsmith@gmail.com>
2021-01-14 22:38:07 -0600

    Refactor around Editor struct

--- src/main.zig
+++ src/main.zig
@@ -13,12 +13,13 @@ pub fn main() anyerror!void {
         const leaked = gpa.deinit();
         if (leaked) panic("leaked memory", null);
     }
-    try enableRawMode();
-    defer disableRawMode();
-    try initEditor();
+    var editor = try Editor.new(&gpa.allocator);
+    defer gpa.allocator.destroy(editor);
+    try editor.enableRawMode();
+    defer editor.disableRawMode();
     while (true) {
-        try editorRefreshScreen(&gpa.allocator);
-        try editorProcessKeyPress();
+        try editor.refreshScreen();
+        try editor.processKeyPress();
         if (editor.shutting_down) break;
     }
     try stdout.writeAll("\x1b[2J");
@@ -40,10 +41,42 @@ const Editor = struct {
     cx: i16,
     cy: i16,
     shutting_down: bool,
+    allocator: *mem.Allocator,
 
-    const Self = *@This();
+    const Self = @This();
 
-    fn moveCursor(self: Self, arrow_key: ArrowKey) void {
+    fn new(allocator: *mem.Allocator) !*Self {
+        const ws = try getWindowSize();
+        var editor = try allocator.create(Self);
+        editor.* = .{
+            .orig_termios = undefined,
+            .rows = ws.rows,
+            .cols = ws.cols,
+            .cx = 0,
+            .cy = 0,
+            .shutting_down = false,
+            .allocator = allocator,
+        };
+        return editor;
+    }
+
+    fn enableRawMode(self: *Self) !void {
+        self.orig_termios = try tcgetattr(stdin_fd);
+        var raw = self.orig_termios;
+        raw.iflag &= ~@as(tcflag_t, BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+        raw.oflag &= ~@as(tcflag_t, OPOST);
+        raw.cflag |= CS8;
+        raw.lflag &= ~@as(tcflag_t, ECHO | ICANON | IEXTEN | ISIG);
+        raw.cc[VMIN] = 0;
+        raw.cc[VTIME] = 1;
+        try tcsetattr(stdin_fd, TCSA.FLUSH, raw);
+    }
+
+    fn disableRawMode(self: *Self) void {
+        tcsetattr(stdin_fd, TCSA.FLUSH, self.orig_termios) catch panic("tcsetattr", null);
+    }
+
+    fn moveCursor(self: *Self, arrow_key: ArrowKey) void {
         switch (arrow_key) {
             .left => {
                 if (self.cx > 0) self.cx -= 1;
@@ -59,33 +92,70 @@ const Editor = struct {
             },
         }
     }
-};
 
-var editor = Editor{
-    .orig_termios = undefined,
-    .rows = undefined,
-    .cols = undefined,
-    .cx = 0,
-    .cy = 0,
-    .shutting_down = false,
-};
-
-fn initEditor() !void {
-    const ws = try getWindowSize();
-    editor.rows = ws.rows;
-    editor.cols = ws.cols;
-}
-
-fn editorProcessKeyPress() !void {
-    const key = try editorReadKey();
-    switch (key) {
-        .char => |ch| switch (ch) {
-            ctrlKey('q') => (&editor).shutting_down = true,
-            else => {},
-        },
-        .arrow_key => |dir| (&editor).moveCursor(dir),
+    fn processKeyPress(self: *Self) !void {
+        const key = try self.readKey();
+        switch (key) {
+            .char => |ch| switch (ch) {
+                ctrlKey('q') => self.shutting_down = true,
+                else => {},
+            },
+            .arrow_key => |dir| self.moveCursor(dir),
+        }
     }
-}
+
+    fn readKey(self: *Self) !Key {
+        const c = try readByte();
+        if (c == '\x1b') {
+            const c1 = readByte() catch return Key{ .char = '\x1b' };
+            if (c1 == '[') {
+                const c2 = readByte() catch return Key{ .char = '\x1b' };
+                switch (c2) {
+                    'A' => return Key{ .arrow_key = .up },
+                    'B' => return Key{ .arrow_key = .down },
+                    'C' => return Key{ .arrow_key = .right },
+                    'D' => return Key{ .arrow_key = .left },
+                    else => {},
+                }
+            }
+        }
+        return Key{ .char = c };
+    }
+
+    fn drawRows(self: *Self, writer: anytype) !void {
+        var y: usize = 0;
+        while (y < self.rows) : (y += 1) {
+            if (y == self.rows / 3) {
+                var welcome = try fmt.allocPrint(self.allocator, "Kilo self -- version {s}", .{kilo_version});
+                defer self.allocator.free(welcome);
+                if (welcome.len > self.cols) welcome = welcome[0..self.cols];
+                var padding = (self.cols - welcome.len) / 2;
+                if (padding > 0) {
+                    try writer.writeAll("~");
+                    padding -= 1;
+                }
+                while (padding > 0) : (padding -= 1) try writer.writeAll(" ");
+                try writer.writeAll(welcome);
+            } else {
+                try writer.writeAll("~");
+            }
+            try writer.writeAll("\x1b[K");
+            if (y < self.rows - 1) try writer.writeAll("\r\n");
+        }
+    }
+
+    fn refreshScreen(self: *Self) !void {
+        var buf = std.ArrayList(u8).init(self.allocator);
+        defer buf.deinit();
+        var writer = buf.writer();
+        try writer.writeAll("\x1b[?25l");
+        try writer.writeAll("\x1b[H");
+        try self.drawRows(writer);
+        try writer.print("\x1b[{d};{d}H", .{ self.cy + 1, self.cx + 1 });
+        try writer.writeAll("\x1b[?25h");
+        try stdout.writeAll(buf.items);
+    }
+};
 
 inline fn ctrlKey(comptime ch: u8) u8 {
     return ch & 0x1f;
@@ -112,24 +182,6 @@ const Key = union(enum) {
     arrow_key: ArrowKey,
 };
 
-fn editorReadKey() !Key {
-    const c = try readByte();
-    if (c == '\x1b') {
-        const c1 = readByte() catch return Key{ .char = '\x1b' };
-        if (c1 == '[') {
-            const c2 = readByte() catch return Key{ .char = '\x1b' };
-            switch (c2) {
-                'A' => return Key{ .arrow_key = .up },
-                'B' => return Key{ .arrow_key = .down },
-                'C' => return Key{ .arrow_key = .right },
-                'D' => return Key{ .arrow_key = .left },
-                else => {},
-            }
-        }
-    }
-    return Key{ .char = c };
-}
-
 const WindowSize = struct {
     rows: u16,
     cols: u16,
@@ -146,54 +198,4 @@ fn getWindowSize() !WindowSize {
     }
 }
 
-fn editorDrawRows(writer: anytype, allocator: *mem.Allocator) !void {
-    var y: usize = 0;
-    while (y < editor.rows) : (y += 1) {
-        if (y == editor.rows / 3) {
-            var welcome = try fmt.allocPrint(allocator, "Kilo editor -- version {s}", .{kilo_version});
-            defer allocator.free(welcome);
-            if (welcome.len > editor.cols) welcome = welcome[0..editor.cols];
-            var padding = (editor.cols - welcome.len) / 2;
-            if (padding > 0) {
-                try writer.writeAll("~");
-                padding -= 1;
-            }
-            while (padding > 0) : (padding -= 1) try writer.writeAll(" ");
-            try writer.writeAll(welcome);
-        } else {
-            try writer.writeAll("~");
-        }
-        try writer.writeAll("\x1b[K");
-        if (y < editor.rows - 1) try writer.writeAll("\r\n");
-    }
-}
-
-fn editorRefreshScreen(allocator: *mem.Allocator) !void {
-    var buf = std.ArrayList(u8).init(allocator);
-    defer buf.deinit();
-    var writer = buf.writer();
-    try writer.writeAll("\x1b[?25l");
-    try writer.writeAll("\x1b[H");
-    try editorDrawRows(writer, allocator);
-    try writer.print("\x1b[{d};{d}H", .{ editor.cy + 1, editor.cx + 1 });
-    try writer.writeAll("\x1b[?25h");
-    try stdout.writeAll(buf.items);
-}
-
 const stdin_fd = io.getStdIn().handle;
-
-fn enableRawMode() !void {
-    editor.orig_termios = try tcgetattr(stdin_fd);
-    var raw = editor.orig_termios;
-    raw.iflag &= ~@as(tcflag_t, BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    raw.oflag &= ~@as(tcflag_t, OPOST);
-    raw.cflag |= CS8;
-    raw.lflag &= ~@as(tcflag_t, ECHO | ICANON | IEXTEN | ISIG);
-    raw.cc[VMIN] = 0;
-    raw.cc[VTIME] = 1;
-    try tcsetattr(stdin_fd, TCSA.FLUSH, raw);
-}
-
-export fn disableRawMode() void {
-    tcsetattr(stdin_fd, TCSA.FLUSH, editor.orig_termios) catch panic("tcsetattr", null);
-}
